97763371a247b95a6725a755d146c455f29733b7
Brad Gessler <bradgessler@gmail.com>
2022-09-28 21:42:16 -0700

    Copy files for generator

--- lib/generators/markdown/install/install_generator.rb
+++ lib/generators/markdown/install/install_generator.rb
@@ -1,3 +1,8 @@
-class Markdown::InstallGenerator < Rails::Generators::NamedBase
+class Markdown::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
+
+  def copy_files
+    directory "app"
+    directory "config"
+  end
 end

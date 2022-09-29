ac3cfde417948f233069f457fed1d221d95207fb
Brad Gessler <bradgessler@gmail.com>
2022-09-28 21:44:21 -0700

    Add more helper methods to Rails Markdown renderer

--- lib/markdown/rails/renderers/rails.rb
+++ lib/markdown/rails/renderers/rails.rb
@@ -4,9 +4,37 @@ module Markdown
       class Rails < Base
         include ::Rails.application.routes.url_helpers
 
+        delegate \
+          :asset_digest_path,
+          :asset_path,
+          :asset_url,
+          :audio_path,
+          :audio_tag,
+          :audio_url,
+          :font_path,
+          :font_url,
+          :image_path,
+          :image_tag,
+          :image_url,
+          :video_path,
+          :video_tag,
+          :video_url,
+          :tag,
+          :content_tag,
+          :render,
+          :request,
+          :turbo_frame_tag,
+          :controller,
+        to: :helpers
+
         def image(link, title, alt)
-          ::ApplicationController.helpers.image_tag link, title: title, alt: alt
+          image_tag link, title: title, alt: alt
         end
+
+        protected
+          def helpers
+            ::ActionController::Base.helpers
+          end
       end
     end
   end

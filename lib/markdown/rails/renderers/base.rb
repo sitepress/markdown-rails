f876f2f25e8a2db62ba6b6ca10c8f9cab5e72448
Brad Gessler <bradgessler@gmail.com>
2022-09-28 21:43:51 -0700

    Make base Markdown renderer have safer defaults

--- lib/markdown/rails/renderers/base.rb
+++ lib/markdown/rails/renderers/base.rb
@@ -3,7 +3,15 @@ module Markdown
     module Renderers
       class Base < Redcarpet::Render::HTML
         def enable
-          []
+          # This is a very restrictive Markdown renderer that errs on the side of safety.
+          # For more details read the docs at https://github.com/vmg/redcarpet#darling-i-packed-you-a-couple-renderers-for-lunch
+          [
+            :filter_html,
+            :no_images,
+            :no_links,
+            :no_styles,
+            :safe_links_only
+          ]
         end
 
         def renderer

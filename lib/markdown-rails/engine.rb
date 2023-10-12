6a9b6817e8188a069ffe8ec30373beaa38564b55
Brad Gessler <bradgessler@gmail.com>
2023-10-11 19:05:43 -0700

    Use the MarkdownRails::Renderer::Rails handler for :md if the user forgets to run the installation command.
    This maintains backwards compatibility with markdown-rails 1.0.

--- lib/markdown-rails/engine.rb
+++ lib/markdown-rails/engine.rb
@@ -1,4 +1,10 @@
 module MarkdownRails
   class Engine < ::Rails::Engine
+    # Check if the `.md` extension has been registered after the
+    # config/initializer files are processed. This makes the `:md`
+    # extension work if the user forgot to install the initializers.
+    config.after_initialize do
+      MarkdownRails::Handler::Markdown.register_default_handler
+    end
   end
 end
--- lib/markdown-rails/handler/markdown.rb
+++ lib/markdown-rails/handler/markdown.rb
@@ -2,6 +2,10 @@ module MarkdownRails
   module Handler
     # We cannot use MarkdownRails because it conflicts with RDiscount's Markdown class
     class Markdown
+      DEFAULT_EXTENSION = :md
+
+      class_attribute :default_extension, default: DEFAULT_EXTENSION
+
       def initialize(&block)
         @markdown = block
       end
@@ -14,6 +18,14 @@ module MarkdownRails
         Array(extensions).each do |extension|
           handler = new &block
           ActionView::Template.register_template_handler extension, handler
+        end
+      end
+
+      # Checks if a handler has been registered with Rails. If it has, then this does nothing. If it hasn't
+      # then this will register a default Rails markdown handler.
+      def self.register_default_handler
+        if default_extension and ActionView::Template::Handlers.extensions.exclude? default_extension
+          handle(default_extension) { MarkdownRails::Renderer::Rails.new }
         end
       end
 

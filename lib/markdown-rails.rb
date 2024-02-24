ae8255f8eb980caf8c8efd595083a8d2a5c2a86c
crespire <hello@crespire.dev>
2024-02-23 21:59:28 -0500

    Add configuration for HTML render options
    The Base renderer was passing in HTML rendering options as extensions,
    so they were not working consistently.
    
    We add a configuration class to make the render options configurable,
    and also update the generated initializer to call out these options.

--- lib/markdown-rails.rb
+++ lib/markdown-rails.rb
@@ -1,19 +1,37 @@
-require "markdown-rails/version"
-require "markdown-rails/engine"
+require 'markdown-rails/version'
+require 'markdown-rails/engine'
 
 module MarkdownRails
   def self.handle(*extensions, &block)
     Handler.handle(*extensions, &block)
   end
 
-  autoload :Handler,   "markdown-rails/handler"
+  autoload :Handler, 'markdown-rails/handler'
 
   module Renderer
-    autoload :Base,       "markdown-rails/renderer/base"
-    autoload :Rails,      "markdown-rails/renderer/rails"
+    class << self
+      def configuration
+        @configuration ||= Configuration.new
+      end
+
+      def configure
+        yield(configuration)
+      end
+    end
+
+    class Configuration
+      attr_accessor :render_options
+
+      def initialize
+        # For option details: https://github.com/vmg/redcarpet?tab=readme-ov-file#darling-i-packed-you-a-couple-renderers-for-lunch
+        @render_options = []
+      end
+    end
+    autoload :Base,       'markdown-rails/renderer/base'
+    autoload :Rails,      'markdown-rails/renderer/rails'
   end
 
   module Helper
-    autoload :Rouge,      "markdown-rails/helper/rouge"
+    autoload :Rouge,      'markdown-rails/helper/rouge'
   end
 end
--- lib/markdown-rails/renderer/base.rb
+++ lib/markdown-rails/renderer/base.rb
@@ -1,28 +1,27 @@
-require "redcarpet"
+require 'redcarpet'
 
 module MarkdownRails
   module Renderer
     class Base < Redcarpet::Render::HTML
-      def enable
-        # This is a very restrictive Markdown renderer that errs on the side of safety.
-        # For more details read the docs at https://github.com/vmg/redcarpet#darling-i-packed-you-a-couple-renderer-for-lunch
-        [
-          :filter_html,
-          :no_images,
-          :no_links,
-          :no_styles,
-          :safe_links_only
-        ]
+      def render_options
+        Renderer.configuration.render_options
       end
 
+      def enable; end
+
       def renderer
-        ::Redcarpet::Markdown.new(self.class, **features)
+        ::Redcarpet::Markdown.new(self.class.new(**render_opts), **features)
       end
 
       private
-        def features
-          Hash[Array(enable).map{ |feature| [ feature, true ] }]
-        end
+
+      def render_opts
+        Hash[Array(render_options).map { |opt| [opt, true] }]
+      end
+
+      def features
+        Hash[Array(enable).map { |feature| [feature, true] }]
+      end
     end
   end
 end
